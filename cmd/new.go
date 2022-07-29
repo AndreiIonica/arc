c7476d5e4d580633b69d3ab640ee3be6a43b284c
AndreiIonica <40264929+andrei-io@users.noreply.github.com>
2022-07-29 16:33:02 +0300

    user input done

--- cmd/new.go
+++ cmd/new.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"scaffold/core/env"
+	"scaffold/core/input"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,6 +12,15 @@ var newProjectCmd = &cobra.Command{
 	Use:   "new",
 	Short: "scaffold a new project",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		userEnv, err := env.GetUserEnv()
+		if err != nil {
+			return err
+		}
+		userAnswers, err := input.AskQuestions(userEnv)
+		if err != nil {
+			return err
+		}
+		fmt.Println(userAnswers)
 		return nil
 	},
 }
--- core/env/user_env.go
+++ core/env/user_env.go
@@ -1,6 +1,48 @@
 package env
 
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
 type UserEnv struct {
 	DefaultProjectName  string
 	AvaialableTemplates []string
+}
+
+func GetUserEnv() (*UserEnv, error) {
+	// TODO: XDG compliance
+	workdingDir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	templatesDir := filepath.Join(home, ".scaffold-templates")
+
+	return getUserEnv(workdingDir, templatesDir)
+}
+
+// Internal implementation for the exported function.
+// The exported function is a thin wrapper around this for testability
+func getUserEnv(workingDir string, templatesDir string) (*UserEnv, error) {
+	e := &UserEnv{}
+
+	workingDir = filepath.Base(workingDir)
+	e.DefaultProjectName = workingDir
+	entries, err := ioutil.ReadDir(templatesDir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			e.AvaialableTemplates = append(e.AvaialableTemplates, entry.Name())
+		}
+	}
+
+	return e, nil
 }
